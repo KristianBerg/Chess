c10c2b29e0f3dd0464143078270040e64c32d422
KristianBerg <kristianberg.jobb@gmail.com>
2015-04-10 12:59:32 +0200

    Divided PlayChess into private methods and continued implementation, added necessary method stubs in ChessBoard

--- src/model/ChessBoard.java
+++ src/model/ChessBoard.java
@@ -1,29 +1,33 @@
 package model;
 
+import pieces.ChessPiece;
 import pieces.Pawn;
 import pieces.Piece;
 
-public class ChessBoard implements Board{
+public class ChessBoard implements Board {
 	private Piece[][] boardState;
 	private static final int BOARDSIZE = 8;
-	
-	public ChessBoard(){
+	private boolean whitesTurn;
+
+	public ChessBoard() {
 		boardState = new Piece[BOARDSIZE][BOARDSIZE];
+		whitesTurn = true;
 	}
-	
-	public boolean movePiece(String from, String to){
+
+	public boolean movePiece(String from, String to) {
 		// TODO Auto-generated method stub
+		ChessPiece piece = parseSquareInput(from);
 		return false;
 	}
 
 	@Override
 	public void setupNewGame() {
 		// TODO Auto-generated method stub
-		for(int i = 0; i < BOARDSIZE; i++){
+		for (int i = 0; i < BOARDSIZE; i++) {
 			boardState[1][i] = new Pawn(1, i, "white");
 			boardState[7][i] = new Pawn(7, i, "black");
 		}
-		
+
 	}
 
 	@Override
@@ -35,7 +39,6 @@ public class ChessBoard implements Board{
 	@Override
 	public void movePiece() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	public String getVisualString() {
@@ -43,9 +46,19 @@ public class ChessBoard implements Board{
 		return null;
 	}
 
-	public String getEndCondition() {
+	public String getCurrentPlayer() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	
+	public boolean staleMate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private ChessPiece parseSquareInput(String from) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/model/playChess.java
+++ src/model/playChess.java
@@ -2,28 +2,30 @@ package model;
 
 import java.util.Scanner;
 
-public class playChess {
+public class PlayChess {
 	private ChessBoard board;
 	private String boardVisual;
 	private Scanner scan;
+	private String endCondition;
 	private boolean gameOver;
 
-	public playChess() {
+	public PlayChess() {
 		board = new ChessBoard();
 		board.setupNewGame();
 		scan = new Scanner(System.in);
 		gameOver = false;
+		endCondition = "";
 	}
 
 	public static void main(String[] args) {
-		playChess game = new playChess();
+		PlayChess game = new PlayChess();
 		game.play();
 		while (true) {
 			System.out.println("Play again? (y/n)");
 			@SuppressWarnings("resource")
 			String playAgain = (new Scanner(System.in)).nextLine();
-			if (playAgain == "y") {
-				game = new playChess();
+			if (playAgain.equals("y")) {
+				game = new PlayChess();
 				game.play();
 			} else {
 				System.exit(0);
@@ -37,20 +39,61 @@ public class playChess {
 			System.out.println(boardVisual);
 			System.out.println("Enter move: ");
 			String nextMove = scan.nextLine();
-			Scanner moveScanner = new Scanner(nextMove);
-			String from = moveScanner.next();
-			String to = moveScanner.next();
-			moveScanner.close();
-			try {
-				board.movePiece(from, to);
-			} catch (moveNotAllowedException e) {
-				System.out.println("Illegal move!");
-			} catch (stillInCheckException e) {
-				System.out.println("King is still in check!");
+			Boolean gameDrawn = checkForDraw(nextMove);
+			Boolean gameForfeited = checkForForfeit(nextMove);
+			if(gameDrawn || gameForfeited){
+				break;
 			}
+			movePiece(nextMove);
 			gameOver = board.checkWinCondition();
 		}
-		String endCondition = board.getEndCondition();
+		checkForStaleMateOrCheckMate(gameOver);
+		printEndCondition();
+	}
+	
+	private void movePiece(String nextMove){
+		Scanner moveScanner = new Scanner(nextMove);
+		String from = moveScanner.next();
+		String to = moveScanner.next();
+		moveScanner.close();
+		try {
+			board.movePiece(from, to);
+		} catch (moveNotAllowedException e) {
+			System.out.println("Illegal move! ");
+		} catch (stillInCheckException e) {
+			System.out.println("King is still in check!");
+		}
+	}
+	
+	private Boolean checkForForfeit(String nextMove) {
+		if(nextMove.equals("forfeit")){
+			endCondition = "forfeit by " + board.getCurrentPlayer();
+		}
+		return false;
+	}
+
+	private Boolean checkForDraw(String nextMove) {
+		if(nextMove.equals("remi")){
+			System.out.println("Accept draw? (y/n)");
+			nextMove = scan.nextLine();
+			if(nextMove.equals("y")){
+				endCondition = "remi";
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private void checkForStaleMateOrCheckMate(boolean gameOver){
+		if(board.staleMate()){
+			endCondition = "stalemate";
+		}
+		else if(gameOver){
+			endCondition = "mate by" + board.getCurrentPlayer();
+		}
+	}
+
+	private void printEndCondition(){
 		if (endCondition.equals("mate by white")) {
 			System.out.println("Check mate, white wins!");
 		} else if (endCondition.equals("mate by black")) {
@@ -61,6 +104,8 @@ public class playChess {
 			System.out.println("Game forfeited, black wins!");
 		} else if (endCondition.equals("forfeit by black")) {
 			System.out.println("Game forfeited, white wins!");
+		} else if (endCondition.equals("stalemate")){
+			System.out.println("Game drawn, stalemate!");
 		}
 	}
 }
